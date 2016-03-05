662ff5fbe004807d813330edc13a7d46d2f6865b
David Beylinov <dave93b@gmail.com>
2016-03-05 22:59:16 +0200

    ---

--- OOPKursach/OOPKursach/MainWindow.xaml.cs
+++ OOPKursach/OOPKursach/MainWindow.xaml.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.Windows.Threading;
 
