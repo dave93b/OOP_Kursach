30450a2043f0b2982dc8a9760074c84eef172fd6
David Beylinov <dave93b@gmail.com>
2016-03-05 22:35:11 +0200

    modified

--- OOPKursach/OOPKursach/MainWindow.xaml.cs
+++ OOPKursach/OOPKursach/MainWindow.xaml.cs
@@ -2,7 +2,9 @@
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
 using System.Windows.Threading;
 
 namespace OOPKursach
@@ -14,6 +16,8 @@ namespace OOPKursach
     {
         private Random _random = new Random();
         private DispatcherTimer _timer = new DispatcherTimer();
+        private double _appearingInterval;
+        private int _totalAppearedItems = 0;
 
         private int _totalPoints = 0, _itemsOnBoard = 0;
 
@@ -26,11 +30,18 @@ namespace OOPKursach
 
         private void DispatcherTimer_Tick(object sender, EventArgs e)
         {
+            _totalAppearedItems++;
+            if (_totalAppearedItems%10 == 0)
+            {
+                _appearingInterval *= 0.9;
+                _timer.Interval = TimeSpan.FromSeconds(_appearingInterval);
+            }
             ShowItem();
         }
 
         private void startButton_Click(object sender, RoutedEventArgs e)
         {
+            _appearingInterval = 1.0;
             canvas.Children.Clear();
             canvas.IsEnabled = true;
             loseLabel.Visibility=Visibility.Collapsed;
@@ -42,30 +53,18 @@ namespace OOPKursach
             
             startButton.IsEnabled = false;
 
-            _timer.Interval = TimeSpan.FromSeconds(0.5);
+            _timer.Interval = TimeSpan.FromSeconds(_appearingInterval);
             _timer.Start();
         }
 
         private void ShowItem()
         {
-            Image image=new Image();
-            BitmapImage bi=new BitmapImage();
-
-            int size = _random.Next(30, 75);
-
-            bi.BeginInit();
-            bi.UriSource=new Uri("1.png",UriKind.Relative);
-            bi.EndInit();
-            image.Height = size;
-            image.Width = size;
-            image.Margin=new Thickness(_random.Next(0,1115),_random.Next(0,560),0,0);
-            image.Source = bi;
-
-            image.MouseLeftButtonDown += Image_MouseLeftButtonDown;
+            DerivedShape2 shape=new DerivedShape2();
+            shape.GetShape().MouseLeftButtonDown += Shape_MouseLeftButtonDown;
             
             if (++_itemsOnBoard<=10)
             {
-                canvas.Children.Add(image);
+                canvas.Children.Add(shape.GetShape());
                 imagesLabel.Content = _itemsOnBoard;
             }
             else
@@ -82,12 +81,47 @@ namespace OOPKursach
             MessageBox.Show("Автор:\nБейлинов Давид\nГруппа: ИКИТ-425");
         }
 
-        private void Image_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        private void Shape_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             imagesLabel.Content = --_itemsOnBoard;
 
             pointsLabel.Content = ++_totalPoints;
-            canvas.Children.Remove((Image)sender);
+            canvas.Children.Remove((Shape)sender);
+        }
+    }
+
+    abstract class BaseShape
+    {
+        protected Shape[] shapes = {new Ellipse(), new Rectangle()};
+
+        public abstract Shape GetShape();
+    }
+
+    class DerivedShape1:BaseShape
+    {
+        Random random=new Random();
+
+        public override Shape GetShape()
+        {
+            Shape shape = shapes[new Random().Next(0, 2)];
+
+            shape.Fill = new SolidColorBrush(Color.FromRgb((byte)random.Next(0, 255), (byte)random.Next(0, 255), (byte)random.Next(0, 255)));
+
+            shape.Stroke = Brushes.Black;
+            shape.StrokeThickness = 3;
+            int size = random.Next(40, 80);
+            shape.Height = size;
+            shape.Width = size;
+            shape.Margin = new Thickness(random.Next(0, 1115), random.Next(0, 550), 0, 0);
+            return shape;
+        }
+    }
+
+    class DerivedShape2:DerivedShape1
+    {
+        public override Shape GetShape()
+        {
+            return base.GetShape();
         }
     }
 }
